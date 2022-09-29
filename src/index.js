aa62b8e59f269731ff5c4aca44c2d5f86ad0589b
Daphineatim <hopedaphine19@gmail.com>
2022-09-29 12:54:29 +0300

    Add webpack, Add style with both css and bootstrap, Create index.js file, Add html section that contains the to-do-list  tasks,

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+const taskList = [
+  {
+    index: 0,
+    description: 'wash the dish',
+    completed: false,
+  },
+  {
+    index: 1,
+    description: 'complete to do list project',
+    completed: false,
+  },
+];
 
- // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+const displayContent = () => {
+  const tasks = document.querySelector('#taskList');
+  taskList.forEach((element) => {
+    tasks.innerHTML += `
+          <div class="border-bottom  m-0 px-3 py-0 d-flex align-items-center justify-content-between" data-id=${element.id}>
+            <div class="form-check mb-0 d-flex align-items-center justify-content-start">
+              <input class="form-check-input border" type="checkbox" value="" id="flexCheckDefault">
+              <label class="form-check-label p-3 m-0" for="flexCheckDefault">
+                <span class="h5 m-0 p-0">${element.description}</span>
+              </label>
+            </div>
+            <button id="three-dots" class="h5 btn m-0 icon">
+              <i class="fa-solid fa-ellipsis-vertical"></i>
+            </button>
+            <button id="trash" class="h5 btn m-0 icon hide">
+              <i class="fa-solid fa-trash-can"></i>
+            </button>
+          </div>
+        `;
+  });
+};
 
-  return element;
-}
-
-document.body.appendChild(component());
+displayContent();
--- webpack.config.js
+++ webpack.config.js
@@ -4,26 +4,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-     clean: true,
+    clean: true,
   },
-  module: {
+   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
     ],
   },
-   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Output Management',
-    }),
-  ],
 };
